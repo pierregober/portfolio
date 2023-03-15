6c1eb37402d884784827f92ac80a1324471f7dfd
CleverlyDone <pierregober@gmail.com>
2023-03-14 22:39:55 -0700

    update form capture

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -170,7 +170,8 @@ function SocialLink({ icon: Icon, ...props }) {
 function Newsletter() {
   return (
     <form
-      action="/thank-you"
+      name="portfolio-capture"
+      data-netlify="true"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
@@ -189,12 +190,14 @@ function Newsletter() {
             type="email"
             placeholder="Email address"
             aria-label="Email address"
+            name="email"
             required
             className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
           />
           <textarea
             rows="5"
             type="text"
+            name="message"
             placeholder="Say something nice..."
             aria-label="Message content"
             required
