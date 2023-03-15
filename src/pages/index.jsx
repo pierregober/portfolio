d424a189583742e16db3ed66af0fb5648903ea89
CleverlyDone <pierregober@gmail.com>
2023-03-14 22:14:03 -0700

    fix download of resume

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -299,10 +299,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <a
-        href="images/Pierre_Gober_Resume.pdf"
-        download="Pierre_Gober_Resume.pdf"
-      >
+      <a href="Pierre_Gober_Resume.pdf" download>
         <Button variant="secondary" className="group mt-6 w-full">
           Download Resume
           <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
