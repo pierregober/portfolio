50a96d5fc551025c673163cefe7f38229402064f
CleverlyDone <pierregober@gmail.com>
2023-03-14 23:23:28 -0700

    swithc the func for the onsubmit handler

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -204,7 +204,7 @@ function Newsletter() {
       name="portfolio-capture"
       data-netlify="true"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(sendMessage)}
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
