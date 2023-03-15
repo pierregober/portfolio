b178b8c4e84c9f5495846ed77b2987c6c24a25e1
CleverlyDone <pierregober@gmail.com>
2023-03-14 23:05:48 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -172,7 +172,6 @@ function Newsletter() {
     <form
       name="portfolio-capture"
       data-netlify="true"
-      action="/"
       method="POST"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
       onSubmit={() => {
