a81be46b2ef33f8984d6aef49a574c004a66589b
CleverlyDone <pierregober@gmail.com>
2023-03-16 16:34:20 -0700

    fix a comma

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -541,7 +541,7 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software Engineer, Real Estate Enthusiast, Marine Corps, Veteran
+            Software Engineer, Real Estate Enthusiast, Marine Corps Veteran,
             Avid Learner
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
