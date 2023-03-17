9745e4ff91d0f7b5c79fbaea48ce2e90474b2ffa
CleverlyDone <pierregober@gmail.com>
2023-03-16 19:12:26 -0700

    adjustments

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -526,8 +526,8 @@ export default function Home() {
     <>
       <Head>
         <title>
-          Pierre Gober - Software Engineer, Real Estate Enthusiast, Marine Corps
-          Veteran, Avid Learner
+          Pierre Gober - Software Engineer, Marine Corps Veteran, Real Estate
+          Enthusiast, Avid Learner
         </title>
         <meta
           name="description"
@@ -541,7 +541,12 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software Engineer, Real Estate Enthusiast, Marine Corps Veteran,
+            Software Engineer,
+            <br />
+            Marine Corps Veteran,
+            <br />
+            Real Estate Enthusiast,
+            <br />
             Avid Learner
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
