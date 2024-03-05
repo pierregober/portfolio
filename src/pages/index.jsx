1f0ac03b3e0cfadf970a4999235f43497769ac76
Pierre Gober <pierregober@gmail.com>
2024-03-04 19:49:31 -0800

    remove resume download

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -348,12 +348,12 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <a href="Pierre_Gober_Resume.pdf" download>
+      {/* <a href="Pierre_Gober_Resume.pdf" download>
         <Button variant="secondary" className="group mt-6 w-full">
           Download Resume
           <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
         </Button>
-      </a>
+      </a> */}
     </div>
   )
 }
