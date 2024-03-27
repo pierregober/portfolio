09c0967146371b7cf5ceff836faaa19767ef41d3
Pierre Gober <pierregober@gmail.com>
2024-03-27 09:09:03 -0700

    Changes copy on the projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -88,7 +88,7 @@ export default function Projects() {
       </Head>
       <SimpleLayout
         title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        intro="I’ve worked on tons of projects over the years but these are the ones that I’m most proud of. I'm still close with some of these projects so you have ideas for how it can be improved, please let me know!"
       >
         <ul
           role="list"
